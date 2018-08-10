034b41c146f7901927e52973240f0e33e73c5c88
S.R. Scott <shaneradio@gmail.com>
2018-08-10 16:11:34 -0400

    added missing ;

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// "require in" http - from npm
+
 const http = require('http');
 
 //establish server object
 
 http.createServer((req, res) => {
-    // issue response
+    // issue our chosen response, of simple text
     res.write("Here's your framework-less server, bub.");
     // end response
     res.end();
@@ -12,4 +14,4 @@ http.createServer((req, res) => {
     listen(3000, () => {
         // note server (server object) is "listening"
         console.log("... server started... listening on port 3000...");
-    })
+    });
